701c4fe8abaa136d4efee39781ec4bba8a0c0913
ajayvarghese003 <ajay.v@razorthink.net>
2019-05-28 14:41:37 +0530

    Changes

--- src/index.js
+++ src/index.js
@@ -16,6 +16,8 @@ if (module.hot) {
       })
 }
 
+console.log(process);
+
 if('serviceWorker' in navigator) {
     navigator.serviceWorker.register('/service-worker.js')
     .then(() => console.log('Service worker registered'))
