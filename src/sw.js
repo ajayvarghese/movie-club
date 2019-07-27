bc624737d5a6e774fa46aa2a87db0f268b3231f8
ajayvarghese003 <ajay.v@razorthink.net>
2019-07-28 01:50:03 +0530

    Adds workbox config to cache js image resources

--- src/sw.js
+++ src/sw.js
@@ -1,4 +1,23 @@
 workbox.precaching.precacheAndRoute(self.__precacheManifest || []);
+
+workbox.routing.registerRoute(
+  /\.(?:js|css)$/,
+  new workbox.strategies.StaleWhileRevalidate(),
+); 
+
+workbox.routing.registerRoute(
+  /\.(?:png|gif|jpg|jpeg|svg)$/,
+  new workbox.strategies.CacheFirst({
+    cacheName: 'images',
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxEntries: 60,
+        maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+      }),
+    ],
+  }),
+); 
+
 self.addEventListener('fetch', function(event) {
   event.respondWith(
     caches.match(event.request)
